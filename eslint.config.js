a89995d35ac6a51a16d8be400bb79e52db447c96
Ferran Comabella <fcomabella@gmail.com>
2025-02-12 13:41:02 +0100

    chore: basic project setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,60 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist", "coverage"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}", "**/__tests_/*.{test,spec}.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "@stylistic": stylistic,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
+      ...reactRefresh.configs.vite.rules,
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+      curly: ["error", "all"],
+      quotes: ["error", "single"],
+      eqeqeq: ["error", "always"],
+      "prefer-const": "error",
+      "@stylistic/arrow-parens": ["error", "always"],
+      "@stylistic/comma-dangle": [
+        "error",
+        {
+          arrays: "always-multiline",
+          objects: "always-multiline",
+          imports: "always-multiline",
+          generics: "ignore",
+        },
+      ],
+      "@stylistic/max-len": [
+        "error",
+        {
+          code: 80,
+          tabWidth: 2,
+          ignoreComments: false,
+          ignoreTrailingComments: true,
+          ignoreUrls: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: false,
+          ignoreRegExpLiterals: true,
+        },
+      ],
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/array-type": ["error", { default: "generic" }],
     },
-  },
-)
+  }
+);
